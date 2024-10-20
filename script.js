3fc88a102a27eea79c3a9b61a24f51065cd49764
Nico <nico.vbiljon@gmail.com>
2024-10-20 11:46:38 +0200

    Update script.js
    Updated the if else statement to be more specific to check if the values are true or false. and added comments .

--- script.js
+++ script.js
@@ -5,16 +5,13 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
     // Check if input starts with 'pet_' and followed by alphanumeric characters
-    if (result == "pet_2015Forrest") {
-        result = "Valid Syntax";
-        return result;
-    } else {
-        result = "Invalid Syntax";
-        return result;
+    // Added if statements to check if the user input is equal or not to the result value.
+    if (input == result) {
+        message = "Valid Syntax";
     }
-
-    document.getElementById("result").innerText = result;
-    console.log(result);
+    if (input != result) {
+        message = "Invalid Syntax";
+    }
+    // Set the id=result innerText value equal to message that will return the text message value if the input is valid or invalid.
+    document.getElementById("result").innerText = message;
 }
-
-console.log(validateSyntax);
