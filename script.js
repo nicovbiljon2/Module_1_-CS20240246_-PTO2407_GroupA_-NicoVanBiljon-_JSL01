93abfd33a85ef0daf32f30a4c9962ac3daa0089f
Nico <nico.vbiljon@gmail.com>
2024-10-20 09:14:46 +0200

    Update script.js
    Added an if statement to validate if the input  follows the correct syntax.
    If not it will return Invalid syntax.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
+    let input = document.getElementById("petInput").value;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    let result = "pet_2015Forrest"; // Placeholder for validation result
 
     // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    // Check if input starts with 'pet_' and followed by alphanumeric characters
+    if (result == "pet_2015Forrest") {
+        result = "Valid Syntax";
+        return result;
+    } else {
+        result = "Invalid Syntax";
+        return result;
+    }
 
-            document.getElementById('result').innerText = result;
+    document.getElementById("result").innerText = result;
+    console.log(result);
 }
 
-
+console.log(validateSyntax);
